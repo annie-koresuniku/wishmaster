4cd330de7a3e5dca906cad6a8edc4d4012a55c5d
annie-koresuniku <koresuniku@fmail.com>
2017-01-12 21:02:11 +0200

    Rename to Wishmaster

---
+++ app/src/main/java/com/koresuniku/wishmaster/ui/UIUtilities.java
@@ -0,0 +1,14 @@
+package com.koresuniku.wishmaster.ui;
+
+
+import android.support.v7.app.ActionBar;
+
+public class UIUtilities {
+    public static void showActionBar(ActionBar ac) {
+        ac.show();
+    }
+
+    public static void hideActionBar(ActionBar ac) {
+        ac.hide();
+    }
+}
